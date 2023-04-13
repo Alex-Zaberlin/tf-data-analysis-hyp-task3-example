2dad547130f82f72deaa537fbe637b6b0e48c8b3
Aleksey Zabelin <55365897+Alex-Zaberlin@users.noreply.github.com>
2023-04-13 23:24:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,6 @@ from scipy.stats import permutation_test
 
 chat_id = 282219367 # Ваш chat ID, не меняйте название переменной
 
-def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    return stats.ttest_ind(x, y)[1] < 0.1 # Ваш ответ, True или False
+def solution(control_data, test_data) -> bool:
+    p_value = 1 - st.norm.cdf((np.mean(test_data) - np.mean(control_data)) / math.sqrt((np.std(control_data)**2 / len(control_data)) + (np.std(test_data)**2 / len(test_data))))
+    return p_value < 0.1
