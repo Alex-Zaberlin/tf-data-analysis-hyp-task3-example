5e0f8a4cef5b2b110a49c6eceda787b47784c6b9
Aleksey Zabelin <55365897+Alex-Zaberlin@users.noreply.github.com>
2023-04-13 20:41:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
+from scipy.stats import permutation_test
 
+chat_id = 282219367 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    return stats.ttest_ind(x, y)[1] < 0.1 # Ваш ответ, True или False
